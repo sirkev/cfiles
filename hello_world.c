3154b459e4eba8dca3a44060db785c4114bbeead
kev <kevinoduor8877@gmail.com>
2024-02-10 18:26:39 +0300

    escape sequences

--- hello_world.c
+++ hello_world.c
@@ -4,7 +4,15 @@ int main(){
     printf("hello world\n");
     printf("hello c");
 
+    // this a comment 
+    /* this is a multiline
+    comment
 
+    /n= new line
+    /t= tab
+    */
+    printf("1\t2\t3\t4\n5\t");
+    printf("this is an ecaped quotes \"double quotes\" and \'single quotes\'");
 
 
     return 0;
